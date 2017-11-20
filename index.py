8e33b0924b34ba5d5584773beecaba0c79f6cfe5
richytruitt <truittrichy@gmail.com>
2017-11-19 19:02:59 -0500

    Able to print a pdf file of all the student ID's

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, redirect, render_template, url_for
+from jinja2 import Environment, FileSystemLoader
 
 from databaseFunctions.database_functions import DbFunctions
 app = Flask(__name__)
@@ -17,6 +18,7 @@ def update():
 
     return render_template('form.html', names = students, books = books)
 
+
 @app.route('/checkout', methods=['POST'])
 def checkout():
     db = DbFunctions()
@@ -33,6 +35,22 @@ def checkout():
         db.update_current_user(i, student_name)
     
     return redirect(url_for("update"))
+
+
+@app.route('/ids')
+def ids():
+    db = DbFunctions()
+    names = db.get_student_names()
+    dictionary = []
+    
+    for i in names:
+        case = {'link': 'labels/students/{}.png'.format(i), 'name': i}
+        dictionary.append(case)
+    
+    print(dictionary)
+
+    return render_template('ids.html', items=dictionary)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5555")
